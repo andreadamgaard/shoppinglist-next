eb9a8128a51407663acabba8ff82940cd4ea4938
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-04-19 11:01:22 +0200

    thing

---
+++ src/app/events/[uuid]/page.jsx
@@ -0,0 +1,29 @@
+export default async function EventPage({ params }) {
+  const uuid = params.uuid;
+
+  let headersList = {
+    Accept: "application/json",
+    apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    Prefer: "return=representation",
+    "Content-Type": "application/json",
+  };
+
+  let response = await fetch("https://szvdpeocqojvcebknuak.supabase.co/rest/v1/events?id=eq." + uuid, { headers: headersList });
+
+  let data = await response.json();
+  const eventInfo = data[0];
+
+  return (
+    <div>
+      <article>
+        <h1>{eventInfo.name}</h1>
+        <dl>
+          <dt className="font-bold">Hvornår:</dt>
+          <dd>{eventInfo.when}</dd>
+        </dl>
+        <p className="font-bold">Nice to know:</p>
+        <p>{eventInfo.description}</p>
+      </article>
+    </div>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Event from "@/app/add-event/page";
 
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      <Event />
     </>
   );
 }
